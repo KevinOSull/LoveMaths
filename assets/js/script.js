e6eb8ef85301e2f55ac9836e81a21891c45e668b
Sephy37 <146596317+Sephy37@users.noreply.github.com>
2025-10-15 16:22:14 +0100

    added functions with functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,7 +25,21 @@ function runGame() {
     }
 }
 
-function checkAnswer() {}
+function checkAnswer() {
+    let userAnswer = parseInt(document.getElementById("answer-box").value);
+    let calculatedAnswer = calculateCorrectAnswer();
+    let isCorrect = userAnswer === calculatedAnswer[0];
+
+    if (isCorrect) {
+        alert("Hey! You got it right! :D");
+    } else {
+        alert(
+            `Awwww.... you answered ${userAnswer}. The correct answer was ${calculatedAnswer[0]}!`
+        );
+    }
+
+    runGame(calculatedAnswer[1]);
+}
 
 function calculateCorrectAnswer() {
     let operand1 = parseInt(document.getElementById("operand1").innerText);
